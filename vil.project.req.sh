047270698dfa34b5c8baa2d518e808176ca1a95c
Rejaul Islam Roky <36339218+rejRoky@users.noreply.github.com>
2022-06-14 23:15:51 +0600

    updated

--- vil.project.req.sh
+++ vil.project.req.sh
@@ -1,15 +1,24 @@
-sudo apt-get install python3.6 &&
 # source env/bin/activate &&
+
+sudo apt-get install python3.6 &&
 sudo apt install python3-pip && 
-# pip3 install -r requirements.txt &&
-pip3 install --upgrade djangorestframework-simplejwt &&
 sudo apt-get install binutils libproj-dev gdal-bin &&
+
+
+# pip3 install -r requirements.txt &&
+
+pip3 install --upgrade djangorestframework-simplejwt &&
 pip3 install django-cors-headers &&
 pip3 install django==3.0.4 &&
 pip3 install django-dynamic-logging &&
 pip3 install django-utils-six &&
-
-sudo apt  install postgresql &&
-sudo systemctl start postgresql &&
-sudo systemctl enable postgresql &&
+pip3 install django-rest-auth &&
+pip3 install drf-api-tracking &&
+pip3 install django-allauth &&
+pip3 install easy-thumbnails &&
+pip3 install django-filer &&
+pip3 install django-anymail &&
+pip3 install django-extensions &&
+pip3 install django-import-export &&
+pip3 install django-admin-sortable2 &&
 pip3 install paho-mqtt 
