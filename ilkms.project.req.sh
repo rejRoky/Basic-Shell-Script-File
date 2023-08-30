09ddec0dceaa566f0bfa044ce9fd640270dae380
rejroky <rejaul.islam.roky@gmail.com>
2023-08-30 12:17:43 +0600

    ilkms

---
+++ ilkms.project.req.sh
@@ -0,0 +1,43 @@
+
+sudo apt-get install python3.10
+sudo apt install python3-pip && 
+
+# pip3 
+pip3 install asgiref==3.6.0
+pip3 install cffi==1.15.1
+pip3 install cryptography
+pip3 install defusedxml==0.7.1
+pip3 install diff-match-patch==20200713
+pip3 install Django==4.1.6
+pip3 install django-admin-interface==0.24.2
+pip3 install django-ckeditor==6.5.1
+pip3 install django-ckeditor-5==0.2.3
+pip3 install django-colorfield==0.8.0
+pip3 install django-cors-headers==3.13.0
+pip3 install django-import-export==3.1.0
+pip3 install django-js-asset==2.0.0
+pip3 install django-phonenumber-field==7.0.2
+pip3 install django-rest-knox==4.2.0
+pip3 install django-tinymce==3.5.0
+pip3 install djangorestframework==3.14.0
+pip3 install djangorestframework-simplejwt==5.2.2
+pip3 install et-xmlfile==1.1.0
+pip3 install MarkupPy==1.14
+pip3 install odfpy==1.4.1
+pip3 install openpyxl==3.1.1
+pip3 install phonenumbers==8.13.7
+pip3 install Pillow==9.4.0
+pip3 install psycopg2-binary==2.9.5
+pip3 install pycparser==2.21
+pip3 install PyJWT==1.7.1
+pip3 install pytz==2022.7.1
+pip3 install PyYAML==6.0
+pip3 install sqlparse==0.4.3
+pip3 install tablib==3.3.0
+pip3 install xlrd==2.0.1
+pip3 install xlwt==1.3.0
+pip3 install django-nested-inline
+pip3 install ip2geotools
+pip3 install google-cloud-vision
+pip3 install pdf2image
+pip3 install opencv-python
--- init_pack_install_setup.sh
+++ init_pack_install_setup.sh
@@ -5,6 +5,17 @@ sudo apt-get dist-upgrade -y &&
 sudo update-grub &&
 sudo apt-get install gdebi -y &&
 sudo apt-get install vlc -y &&
-sudo apt-get install git -y &&
-sudo apt autoremove -y
+sudo apt-get install postgresql &&
+sudo apt autoremove -y &&
 
+sudo apt update && sudo apt install -y \
+    build-essential \
+    zip \
+    nano \
+    vim \
+    unzip \
+    git \
+    curl\
+    apt-utils \
+    libpq-dev \
+    sudo
